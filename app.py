0d5b5cb75080910f7a4a7ae9f786bd6174a8645d
s1f102202998 <s1f102202998@iniad.org>
2022-06-30 14:14:13 +0900

    update app.py & vote.html

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+kinoko_count=3
+takenoko_count=5
+messages=['Hi1','Hi2']
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    #進捗グラフの更新
+    kinoko_percent=kinoko_count/(kinoko_count+takenoko_count)*100
+    takenoko_percent=takenoko_count/(kinoko_count+takenoko_count)*100
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
